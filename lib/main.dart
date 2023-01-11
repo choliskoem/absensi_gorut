f6a8208115502a5686f2c25c8abd992745730503
Abas Djumadi <abasd04@gmail.com>
2023-01-11 23:27:11 +0800

    include crashlitic

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,25 @@ import 'dart:io';
 import 'package:absensi/pages/HttpOverrides.dart';
 import 'package:absensi/pages/splashscreen.dart';
 import 'package:absensi/pages/splashscreennav.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
   HttpOverrides.global = MyHttpOverrides();
   await GetStorage.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
